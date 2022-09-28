859eb40f7d051a118c7f57585ef0c49e25217da9
myszol321 <myszola321@wp.pl>
2022-09-28 02:08:05 +0200

    rating

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import foto from '../images/foto.png'
-import UserEventList from './EventList'
 import Event from './Event'
-// const axios = require('axios').default;
 
 export default function ProfilePage(props) {
     
     const [userInfo, setUserInfo] = React.useState({})
-
+    const [rating, setRating] = React.useState("")
     const [eventData, setEventData] = React.useState([])
 
     React.useEffect(() => {
@@ -16,31 +14,16 @@ export default function ProfilePage(props) {
             const profileId = pathname.slice(13)
             if(props.userInfo.id.toString() === profileId) {
                 setUserInfo(props.userInfo)
-
-                const response_events1 = await fetch(`http://localhost:4000/events/user/${profileId}`)
-                const data_events1 = await response_events1.json()
-                const events = data_events1.map(entry => {
-                    return(
-                        <Event
-                            key={entry.id}
-                            {...entry}
-                            handleClick={props.handleClick}
-                        />
-                    )
-                })
-                setEventData(events)
-
             } 
             else {
-                console.log(profileId)
-                const response_user2 = await fetch(`http://localhost:4000/users/${profileId}`)
-                const data_user2 = await response_user2.json()
-                setUserInfo(data_user2[0])
+                const response_user = await fetch(`http://localhost:4000/users/${profileId}`)
+                const data_user = await response_user.json()
+                setUserInfo(data_user[0])
+            }
 
-                const response_events2 = await fetch(`http://localhost:4000/events/user/${profileId}`)
-                const data_events2 = await response_events2.json()
-                console.log(data_events2)
-                const events = data_events2.map(entry => {
+                const response_events = await fetch(`http://localhost:4000/events/user/${profileId}`)
+                const data_events = await response_events.json()
+                const events = data_events.map(entry => {
                     return(
                         <Event
                             key={entry.id}
@@ -50,10 +33,48 @@ export default function ProfilePage(props) {
                     )
                 })
                 setEventData(events)
-            }
+
+                const response_stars = await fetch(`http://localhost:4000/users/${profileId}/stars`)
+                const data_stars = await response_stars.json()
+                setRating(data_stars[0].average_rating.slice(0,4))
         }
         setAll();
     }, [])
+
+    const changeRating = (e) => {
+        console.log(e.target.value)
+        console.log(props.userInfo.id)
+        console.log(userInfo.id)
+
+        if(userInfo.id === props.userInfo.id) {
+            alert('Nie możesz ocenić siebie')
+        } else {
+            const fetchedData = fetch(`http://localhost:4000/users/${userInfo.id}/stars`, {
+                method: 'POST',
+                mode: 'cors',
+                body: JSON.stringify({my_id: props.userInfo.id, stars: e.target.value}),
+                headers: { "Content-Type": "application/json" }
+            })
+            .then((response) => {
+                if (response.ok) {
+                    console.log(response)
+                    
+                } else {
+                    alert(response.statusText)
+                    console.log(response.statusText)
+                }
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        }
+        async function getRating() {
+            const response_stars = await fetch(`http://localhost:4000/users/${userInfo.id}/stars`)
+            const data_stars = await response_stars.json()
+            setRating(data_stars[0].average_rating.slice(0,4))
+        }
+        getRating();
+    }
 
     return (
         <div className="profile">
@@ -64,18 +85,18 @@ export default function ProfilePage(props) {
             </div>
             <div className="profile--rating">
                 <div className="profile--rating--stars">
-                    <input type="radio" id="star5" name="rate" value="5" />
+                    <input type="radio" id="star5" name="rate" value="5" checked={rating === 5} onChange={changeRating}/>
                     <label for="star5" title="text">5 stars</label>
-                    <input type="radio" id="star4" name="rate" value="4" />
+                    <input type="radio" id="star4" name="rate" value="4" checked={rating >= 4 && rating < 5} onChange={changeRating}/>
                     <label for="star4" title="text">4 stars</label>
-                    <input type="radio" id="star3" name="rate" value="3" />
+                    <input type="radio" id="star3" name="rate" value="3" checked={rating >= 3 && rating < 4} onChange={changeRating}/>
                     <label for="star3" title="text">3 stars</label>
-                    <input type="radio" id="star2" name="rate" value="2" />
+                    <input type="radio" id="star2" name="rate" value="2" checked={rating >= 2 && rating < 3} onChange={changeRating}/>
                     <label for="star2" title="text">2 stars</label>
-                    <input type="radio" id="star1" name="rate" value="1" />
+                    <input type="radio" id="star1" name="rate" value="1" checked={rating >= 1 && rating < 2} onChange={changeRating}/>
                     <label for="star1" title="text">1 star</label>
                 </div>
-                <p className="profile--rating--number">ocenka</p>
+                <p className="profile--rating--number">{rating}</p>
             </div>
             
             <p className="profile--description">
